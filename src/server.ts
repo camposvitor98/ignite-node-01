312ef8d9709da8208f1a64bde1a66bc3730e73a4
campos.vitor98 <campos.vitor98@gmail.com>
2023-08-10 14:48:39 -0300

    knex config

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import fastify from "fastify";
+import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
-app.get("/hello", () => {
-  return "Hello world!";
+app.get("/hello", async () => {
+  return await knex("sqlite_schema").select(`*`);
 });
 
 app
   .listen({
-    port: 9898,
+    port: env.PORT,
   })
   .then(() => console.log("listening on port 9898"));
